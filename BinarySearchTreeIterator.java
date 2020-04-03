a13fc843b30492ad4a6a89fb9bda9b0ade749c1a
Mohiul Alam <mohiulalamprince@yahoo.com>
2020-04-03 21:13:20 +0200

    April 03, 2020 21:13

---
+++ BinarySearchTreeIterator.java
@@ -0,0 +1,25 @@
+class BSTIterator {
+
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        return 1;
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return true;
+    }
+}
+
+
+public class BinarySearchTreeIterator {
+    public static void main(String args[]) {
+
+    }
+
+
+}
