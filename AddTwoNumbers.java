2d0ca6e4b462675c22684af0f5f5012d87b09312
Mohiul Alam <mohiulalamprince@yahoo.com>
2020-08-30 19:08:21 +0200

    August 30, 2020 19.08

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,48 +1,42 @@
 
 class AddTwoNumbers {
 
-        static ListNode head;
+        static ListNode head = null;
 
         public static void main(String args[]) {
-            ListNode l1 = new ListNode(5);
-            ListNode l2 = new ListNode(6);
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next =new ListNode(3);
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
 
             ListNode result = addTwoNumbers(l1, l2);
             print(result);
         }
 
         public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode result = null;
+            ListNode result = new ListNode(0);
             head = result;
 
             int carry = 0;
-            while (l1 != null && l2 != null) {
-                int sum = carry + l1.val + l2.val;
-                carry = sum % 10;
-                ListNode newNode = new ListNode(sum / 10);
+            while (l1 != null || l2 != null) {
+                int p = (l1 != null) ? l1.val : 0;
+                int q = (l2 != null) ? l2.val : 0;
+                int sum = carry + p + q;
+                carry = sum / 10;
+                ListNode newNode = new ListNode(sum % 10);
 
-                if (result == null)
-                    result = newNode;
-                else {
-                    result.next = newNode;
-                }
+                result.next = newNode;
+                result = result.next;
 
                 l1 = l1.next;
                 l2 = l2.next;
             }
 
-            while (l1 != null) {
-                result.next = new ListNode((carry + l1.val) % 10);
-                carry = (l1.val + carry) / 10;
-                l1 = l1.next;
-            }
-
-            while (l2 != null) {
-                result.next = new ListNode((carry + l2.val) % 10);
-                carry = (l2.val + carry) / 10;
-                l2 = l2.next;
-            }
-            return head;            
+            if (carry == 1)
+                result.next = new ListNode(carry);
+            return head.next;
         }
 
         public static void print(ListNode root) {
