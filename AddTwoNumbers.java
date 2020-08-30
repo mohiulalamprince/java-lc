1056dbfbef95b28222308d57609d944633a73e8b
Mohiul Alam <mohiulalamprince@yahoo.com>
2020-08-30 19:28:51 +0200

    August 30, 2020 19.08

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -30,8 +30,8 @@ class AddTwoNumbers {
                 result.next = newNode;
                 result = result.next;
 
-                l1 = l1.next;
-                l2 = l2.next;
+                l1 = (l1 != null) ? l1.next : l1;
+                l2 = (l2 != null) ? l2.next : l2;
             }
 
             if (carry == 1)
